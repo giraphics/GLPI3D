1d224ef8725421dd7f89c97e9f5283c32cb2f947
parminder.vaio <parminder@qi.sg>
2015-07-14 21:29:53 +0800

    Flat rendering system with plain lists for initialization and rendering. Matrix and Geometry buffer class create in order to remove the redundancy of the Common from Specifics. Demo are made key operated for better switching between samples

--- Source/common/IModel.h
+++ Source/common/IModel.h
@@ -6,6 +6,42 @@
 #ifndef __IMODEL__
 #define __IMODEL__
 
+class TransformationMatrix{
+public:
+	TransformationMatrix(){}
+	~TransformationMatrix(){}
+	/*!
+	Set the Model view projection matrix here.
+	*/
+	virtual void SetModelMat(glm::mat4* ModelMat){ ModelMatrix = ModelMat; }
+
+	/*!
+	Set the Model view projection matrix here.
+	*/
+	virtual void SetViewMat(glm::mat4* ViewMat){ ViewMatrix = ViewMat; }
+
+	/*!
+	Set the Model view projection matrix here.
+	*/
+	virtual void SetProjectionMat(glm::mat4* ProjectionMat){ ProjectionMatrix = ProjectionMat; }
+	glm::mat4* getTempMatrix(){return &tempMatrix;}
+	glm::mat4* getTempMVPMatrix(){return &tempMVPMatrix;}
+protected:
+	// Model Matrix
+	glm::mat4* ModelMatrix;
+	
+	// View Matrix
+	glm::mat4* ViewMatrix;
+
+	// Projection Matrix
+	glm::mat4* ProjectionMatrix;
+
+	// This is a temporary matrix which is use to store temporary calculation like MVP, MV, normalMatrix 
+	// and send to the shader program. This way we avoid any temporary local matrices on the stack.
+	glm::mat4 tempMatrix;
+	glm::mat4 tempMVPMatrix;
+};
+
 class IModel
 {
 public:
@@ -27,19 +63,20 @@ public:
 	/*!
 	Set the Model view projection matrix here.
 	*/
-	virtual void SetModelMat(glm::mat4* ModelMat){ ModelMatrix = ModelMat; }
+	//virtual void SetModelMat(glm::mat4* ModelMat){ ModelMatrix = ModelMat; }
 
 	/*!
 	Set the Model view projection matrix here.
 	*/
-	virtual void SetViewMat(glm::mat4* ViewMat){ ViewMatrix = ViewMat; }
+	//virtual void SetViewMat(glm::mat4* ViewMat){ ViewMatrix = ViewMat; }
 
 	/*!
 	Set the Model view projection matrix here.
 	*/
-	virtual void SetProjectionMat(glm::mat4* ProjectionMat){ ProjectionMatrix = ProjectionMat; }
+	//virtual void SetProjectionMat(glm::mat4* ProjectionMat){ ProjectionMatrix = ProjectionMat; }
 
 protected:
+/*
 	// Model Matrix
 	glm::mat4* ModelMatrix;
 	
@@ -52,7 +89,7 @@ protected:
 	// This is a temporary matrix which is use to store temporary calculation like MVP, MV, normalMatrix 
 	// and send to the shader program. This way we avoid any temporary local matrices on the stack.
 	glm::mat4 tempMatrix;
-	glm::mat4 tempMVPMatrix;
+	glm::mat4 tempMVPMatrix;*/
 
 	// Store the ID of the current shader program handle locally in class object
 	unsigned int ProgramID;
--- Source/common/MeshObject.cpp
+++ Source/common/MeshObject.cpp
@@ -21,7 +21,16 @@ MeshObject::~MeshObject()
 */
 void MeshObject::Initialize()
 {
-	specificMesh->Initialize();
+	if (scene()->getRenderer()->getRendererType() == PluginType::OPENGLES20_STATIC_PLUGIN){
+		transformationMethod = &MeshObject::setTransformationForOpenGLES20Pipeline;
+	}
+	else{
+		// Handle other pipelines 
+	}
+
+	//specificMesh->Initialize();
+	scene()->getRenderer()->initFlatList.push_back(specificMesh);
+
     Model::Initialize();
     return;
 }
@@ -42,10 +51,12 @@ void MeshObject::Render(bool (*customRender)())
     {
         TransformObj->TransformPushMatrix(); // Local Level
         ApplyModelsLocalTransformation();
-		specificMesh->SetModelMat(TransformObj->TransformGetModelMatrix());
-		specificMesh->SetViewMat(TransformObj->TransformGetViewMatrix());
-		specificMesh->SetProjectionMat(TransformObj->TransformGetProjectionMatrix());
-		specificMesh->Render();
+		//((GLES20MeshLoader*)specificMesh)->SetModelMat(TransformObj->TransformGetModelMatrix());
+		//((GLES20MeshLoader*)specificMesh)->SetViewMat(TransformObj->TransformGetViewMatrix());
+		//((GLES20MeshLoader*)specificMesh)->SetProjectionMat(TransformObj->TransformGetProjectionMatrix());
+		(this->*transformationMethod)();
+		scene()->getRenderer()->renderFlatList.push_back(specificMesh);
+		//specificMesh->Render();
         TransformObj->TransformPopMatrix(); // Local Level
     }
 
@@ -56,4 +67,14 @@ void MeshObject::Render(bool (*customRender)())
 void MeshObject::SetProgram(unsigned int ID)
 {
    specificMesh->SetProgram(ID);
+}
+
+void MeshObject::setTransformationForOpenGLES20Pipeline(){
+	GLES20MeshLoader* gles20Mesh = ((GLES20MeshLoader*)specificMesh);
+	*gles20Mesh->getTempMatrix() = *TransformObj->TransformGetViewMatrix() * *TransformObj->TransformGetModelMatrix();
+	*gles20Mesh->getTempMVPMatrix() = *TransformObj->TransformGetProjectionMatrix() * *TransformObj->TransformGetViewMatrix() * *TransformObj->TransformGetModelMatrix();
+	
+	//gles20Mesh->SetModelMat(TransformObj->TransformGetModelMatrix());
+	//gles20Mesh->SetViewMat(TransformObj->TransformGetViewMatrix());
+	//gles20Mesh->SetProjectionMat(TransformObj->TransformGetProjectionMatrix());
 }
